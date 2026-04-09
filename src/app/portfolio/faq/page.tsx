f43c77586992e2156d7ca544440ea746e9fdf8f8
cabitibaly <bitibalyclementalex@gmail.com>
2026-04-09 12:31:16 +0000

    petite modif

--- src/app/portfolio/faq/page.tsx
+++ src/app/portfolio/faq/page.tsx
@@ -5,10 +5,27 @@ import FaqCard from "@/components/portfolio/cards/faqCard"
 const Faq = () => {    
 
     return (
-        <div className="p-28 bg-[#111113] w-screen h-screen flex flex-col items-center justify-center">
+        <div className="p-28 bg-[#111113] w-screen min-h-screen flex flex-col items-center justify-start">
             <FaqCard />
             <FaqCard />
             <FaqCard />
+            <div className="w-full flex-col items-center justify-start">
+                <div className="w-full border-t border-white py-6 collapse rounded-none">
+                    <input type="radio" name="my-accordion-1" />
+                    <div className="collapse-title text-4xl text-white font-medium uppercase">How do I create an account?</div>
+                    <div className="collapse-content text-base text-left text-white font-normal uppercase">Click the "Sign Up" button in the top right corner and follow the registration process.</div>
+                </div>
+                <div className="w-full border-t border-white py-6 collapse rounded-none">
+                    <input type="radio" name="my-accordion-1" />
+                    <div className="collapse-title text-4xl text-white font-medium uppercase">How do I create an account?</div>
+                    <div className="collapse-content text-base text-left text-white font-normal uppercase">Click the "Sign Up" button in the top right corner and follow the registration process.</div>
+                </div>
+                <div className="w-full border-t border-white py-6 collapse rounded-none">
+                    <input type="radio" name="my-accordion-1" />
+                    <div className="collapse-title text-4xl text-white font-medium uppercase">How do I create an account?</div>
+                    <div className="collapse-content text-base text-left text-white font-normal uppercase">Click the "Sign Up" button in the top right corner and follow the registration process.</div>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/app/portfolio/snap-scroll/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+const SnapScroll = () => {
+
+    return (
+        <div className="container">
+            <div className="section section-01"><h2>one</h2></div>
+            <div className="section section-02"><h2>two</h2></div>
+            <div className="section section-03"><h2>three</h2></div>
+        </div>
+    )
+}
+
+export default SnapScroll
